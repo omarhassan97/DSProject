8b0a0edf682766744c2efa26cd16f8075533a027
omarhassan97 <omarhassan97@users.noreply.github.com>
2019-12-16 20:34:36 +0200

    Editing the Order error

--- Project/Project/Events/ArrivalEvent.cpp
+++ Project/Project/Events/ArrivalEvent.cpp
@@ -13,17 +13,17 @@ void ArrivalEvent::Execute(Restaurant* pRest)
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
 	if (OrdType == TYPE_NRM)
 	{
-		Order newOrder(this->OrderID, this -> OrdType);
+		Order* newOrder = new Order(this->OrderID, this -> OrdType);
 		pRest->NormalOrders.enqueue(newOrder);
 	}
 	else if (OrdType == TYPE_VEG)
 	{
-		Order newOrder(this->OrderID, this->OrdType);
+		Order* newOrder = new Order(this->OrderID, this->OrdType);
 		pRest->VeganOrders.enqueue(newOrder);
 	}
 	else if (OrdType == TYPE_VIP)//TO be edited
 	{
-		Order newOrder(this->OrderID, this->OrdType);
+		Order* newOrder = new Order(this->OrderID, this->OrdType);
 		pRest->VeganOrders.enqueue(newOrder);
 	}
 		
